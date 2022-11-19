a1717ab1b8635524bdd6b52c18dea989852c3915
Bekti <bektibayev.amirali@gmail.com>
2022-11-20 00:49:31 +0100

    even or odd

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -39,3 +39,16 @@ function positiveSum(arr) {
  }
 return sum;
 }
+
+/* 3)
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function evenOrOdd(number) {
+  if(number % 2 == 0) {
+    return 'Even'
+  } else {
+    return "Odd"
+  }
+}
+
