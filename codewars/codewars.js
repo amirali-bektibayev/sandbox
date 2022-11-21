615ef168a2ad49af256dc71128b9a15c7bc7d7c8
Bekti <bektibayev.amirali@gmail.com>
2022-11-21 13:05:50 +0100

    return the additive inverse of each

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -52,3 +52,13 @@ function evenOrOdd(number) {
   }
 }
 
+/* 4)
+Given a set of numbers, return the additive inverse of each. 
+Each positive becomes negatives, and the negatives become positives.
+*/
+
+function invert(array) {
+   return array.map(num => num * -1)
+}
+
+
